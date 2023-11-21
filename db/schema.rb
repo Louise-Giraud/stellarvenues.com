215668fba22f0148b3ddf4223d09ec9950aefbe9
Kat Syddall <kat.syddall@gmail.com>
2023-11-21 12:43:39 +0100

    created venue model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_152244) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_112031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,4 +27,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_152244) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "capacity"
+    t.bigint "user_id", null: false # where do we change this to host_id??
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_venues_on_user_id"
+  end
+
+  add_foreign_key "venues", "users"
 end
