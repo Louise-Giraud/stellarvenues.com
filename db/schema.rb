a7a9d7ab9a063a9bcf041f91f3c000bb2aece829
Louise Giraud <louisegiraud7@gmail.com>
2023-11-21 16:56:30 +0100

    booking model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_145726) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_154829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.bigint "venue_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "booked", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["venue_id"], name: "index_bookings_on_venue_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,5 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_145726) do
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "venues"
   add_foreign_key "venues", "users"
 end
