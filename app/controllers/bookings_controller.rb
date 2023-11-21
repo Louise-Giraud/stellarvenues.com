8f908d82b9fd8bcb7f21a85b949c60b57b8d89d6
Louise Giraud <louisegiraud7@gmail.com>
2023-11-21 17:22:23 +0100

    booking model and controller update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,18 @@
 class BookingsController < ApplicationController
-  def index
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.create(booking_params)
+    @venue = Venue.find(params[:venue_id])
+    @booking.venue = @venue
+    @booking.save!
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :current_user)
   end
 end
