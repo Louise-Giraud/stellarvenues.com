b146e20e22adc9d7449b90a9dbb71f719d5af215
Louise Giraud <louisegiraud7@gmail.com>
2023-11-22 14:52:01 +0100

    create booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,15 +3,16 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
-  def new
-    @booking = Booking.new
-  end
-
   def create
     @booking = Booking.create(booking_params)
     @venue = Venue.find(params[:venue_id])
     @booking.venue = @venue
-    @booking.save!
+    @booking.user = current_user
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
