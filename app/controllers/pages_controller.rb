fe24f416df391a99fdc50697e69088709d03a9fe
Kat Syddall <kat.syddall@gmail.com>
2023-11-22 16:17:52 +0100

    my pending bookings front end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
-pendingbookingscontroller
   def dashboard
     @pending_bookings_as_owner = current_user.bookings_as_owner.where(status: 0)
     @confirmed_bookings_as_owner = current_user.bookings_as_owner.where(status: 1)
     @bookings_as_booker = current_user.bookings.where(status: 1)
+  end
 end
