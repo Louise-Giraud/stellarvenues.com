31a60fb215f6d0e7fc4582bf05016b4da62acf03
Louise Giraud <louisegiraud7@gmail.com>
2023-11-23 12:40:45 +0100

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,19 @@ user = User.create(email: "user2445@gmail.com", password: "123456", username: "a
 user1 = User.create(email: "user2446@gmail.com", password: "123456", username: "kat_user")
 user2 = User.create(email: "user2447@gmail.com", password: "123456", username: "louise_user")
 
+capacity = [100, 130, 200, 500, 1600, 4500, 3000, 30000]
+galaxies = ["Andromeda", "Black Eye Galaxy", "Bode's Galaxy", "Cartwheel Galaxy", "Cigar Galaxy", "Comet Galaxy", "Cosmos Redshift", "7 Hoag's Object", "Large Magellanic", "Cloud Small Magellanic", "Cloud Mayall's Object", "Milky Way", "Pinwheel Galaxy", "Sombrero Galaxy", "Sunflower Galaxy"]
 
-puts 'Creating 10 fake venues...'
+puts 'Creating 15 fake galaxies...'
 
-10.times do
+15.times do
   venue = Venue.new(
-    name:    Faker::Space.galaxy,
+    name: galaxies.sample,
     address: Faker::Address.full_address,
-    capacity:  rand(20..1500),
+    capacity:  capacity.sample,
+    logistic_partner: Faker::Space.company,
+    access: Faker::Space.launch_vehicle,
+    available_ressources: Faker::Science.element,
     user: user,
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   )
