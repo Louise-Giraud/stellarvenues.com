62cc4c6eae4741fee3aec9d647b88198dd03d944
Louise Giraud <louisegiraud7@gmail.com>
2023-11-24 12:46:21 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,14 @@
 require 'faker'
 Venue.destroy_all
 User.destroy_all
-user = User.create(email: "user2445@gmail.com", password: "123456", username: "amazing_user")
-user1 = User.create(email: "user2446@gmail.com", password: "123456", username: "kat_user")
-user2 = User.create(email: "user2447@gmail.com", password: "123456", username: "louise_user")
+user = User.new(email: "user2445@gmail.com", password: "123456", username: "amazing_user")
+pic = URI.open('app/assets/images/Thumb-ET.webp')
+user.photo.attach(io: pic, filename: 'Thumb-ET.webp', content_type: 'image/webp')
+user.save
+
+
+# user1 = User.create(email: "user2446@gmail.com", password: "123456", username: "kat_user")
+# user2 = User.create(email: "user2447@gmail.com", password: "123456", username: "louise_user")
 
 capacity = [100, 130, 200, 500, 1600, 4500, 3000, 30000]
 galaxies = ["Andromeda", "Black Eye Galaxy", "Bode's Galaxy", "Cartwheel Galaxy", "Cigar Galaxy", "Comet Galaxy", "Cosmos Redshift", "7 Hoag's Object", "Large Magellanic", "Cloud Small Magellanic", "Cloud Mayall's Object", "Milky Way", "Pinwheel Galaxy", "Sombrero Galaxy", "Sunflower Galaxy"]
